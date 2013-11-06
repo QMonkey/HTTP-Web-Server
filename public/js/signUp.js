10a48f59ae397786c07c7da9e7c1753ba8bb9269
QMKQMonkey <QMKQMonkey@gmail.com>
2013-11-06 12:23:43 +0800

    Finish signIn and signUp.

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,8 +1,11 @@
 window.addEventListener("load",function() {
 	document.getElementById("signUpForm").addEventListener("submit",function(eve) {
+		var errmsg = document.getElementById("errmsg");
+		errmsg.style.display = "none";
 		if(document.getElementById("psw").value != 
 			document.getElementById("verifyPsw").value) {
 				eve.preventDefault();
+				errmsg.style.display = "inline";
 		}
 	});
 });
